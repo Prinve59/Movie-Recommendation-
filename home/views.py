de3de05a2bcc270bd03056abdef53646efa400bf
Prinve59 <149373739+Prinve59@users.noreply.github.com>
2025-08-27 02:54:11 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -6,13 +6,7 @@ import requests
 movie_lst=pickle.load(open("movies.pkl",'rb'))
 similarity=pickle.load(open("simil.pkl",'rb'))
 
-def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("poster_path", None)
-    return None
+
 
 
 def rec(movie):
@@ -27,15 +21,14 @@ def rec(movie):
     for i,sim_sc in mov_list:
         if i==movies_idx:
             continue
-        poster_path=movie_lst.iloc[i].movie_id
+        poster_path=movie_lst.iloc[i].posters
         title = movie_lst.iloc[i].title
-        rating = movie_lst.iloc[i].vote_average
+        rating = movie_lst.iloc[i].rating
         final_sc=sim_sc*(0.7+0.3 * (rating / 10))
         rec_m.append((title,rating, final_sc,poster_path))
     rec_m = sorted(rec_m, key=lambda x: x[2], reverse=True)[:10]
     final_rec = []
-    for title, rating, final_sc, movie_id in rec_m:
-        poster_path = fetch_poster(movie_id)
+    for title, rating, final_sc, poster_path in rec_m:
         final_rec.append((title,poster_path,rating, final_sc))
 
     return final_rec
